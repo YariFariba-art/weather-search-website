414b08fffe9c3076d657c96c5742dd1808109b48
Fariba Yari <fariba.yari2019@gmail.com>
2024-12-31 20:53:31 +0430

    Axios and API Integration

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+function showTemp (response){
+    let temperatureElement = document.querySelector("#weather-degree");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#currentCity"); 
+    cityElement.innerHTML =  response.data.city;
+   temperatureElement.innerHTML= Math.round(temperature);
+
+}
+
+function findCity (city){
+    let apiKey = "ef6e0a4b30090aob6a88ad3faec013at";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showTemp);
+}
+
 function searchCity (event) {
     event.preventDefault ();
     let cityInputElement = document.querySelector("#search");
-    let cityElement = document.querySelector("#currentCity"); 
-    cityElement.innerHTML = cityInputElement.value.trim();
+    findCity(cityInputElement.value.trim());
 }
 
 let searchFormElement= document.querySelector("#city");
